165a8b30da86be115c91ac1e9309b230b2f7ff30
ulasanekonomi-collab <ulasanekonomi@gmail.com>
2026-04-28 23:49:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -63,8 +63,5 @@ else:
 st.markdown("---")
 st.caption("Dikembangkan oleh Yuhka Sundaya | Ekonomi Pembangunan Unisba | 2026")
 col1, col2 = st.columns(2)
-
-with col1:
-    st.image("unisba.png", width=120)
 st.markdown("---")
 st.caption("Dikembangkan oleh Yuhka Sundaya | Ekonomi Pembangunan Unisba | 2026")
